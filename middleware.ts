7171e5efdcd73742cc16ca8c234484b0c229d809
marastac <132803403+marastac@users.noreply.github.com>
2025-10-04 02:46:19 -0500

    Fix: middleware and auth final version

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,2 @@
-// /middleware.ts
-export { auth as middleware } from '@/auth'; // si tu alias "@/" no funciona, usa './auth'
-
-export const config = {
-  matcher: ['/dashboard/:path*'], // protege todo /dashboard
-};
+export { auth as middleware } from 'next-auth';
+export const config = { matcher: ['/dashboard/:path*'] };
